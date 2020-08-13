b79d00b79bcc75b4f7b36b7d609fbb5941d70a5d
Zachary Bell <z.bell@me.com>
2020-08-12 20:59:59 -0400

    use simple form to submit to DB

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,11 +10,13 @@ class PlacesController < ApplicationController
   # GET /places/1
   # GET /places/1.json
   def show
+    @place = Place.find(params[:id])
   end
 
   # GET /places/new
   def new
-    @place = Place.new
+
+
   end
 
   # GET /places/1/edit
@@ -24,17 +26,8 @@ class PlacesController < ApplicationController
   # POST /places
   # POST /places.json
   def create
-    @place = Place.new(place_params)
-
-    respond_to do |format|
-      if @place.save
-        format.html { redirect_to @place, notice: 'Place was successfully created.' }
-        format.json { render :show, status: :created, location: @place }
-      else
-        format.html { render :new }
-        format.json { render json: @place.errors, status: :unprocessable_entity }
-      end
-    end
+    @place = Place.create(place_params)
+    redirect_to places_path
   end
 
   # PATCH/PUT /places/1
@@ -67,8 +60,8 @@ class PlacesController < ApplicationController
       @place = Place.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Only allow a list of trusted parameters through
     def place_params
-      params.fetch(:place, {})
+      params.require(:place).permit(:park_name, :address, :zip_code)
     end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,7 @@
 class Place < ApplicationRecord
+  
+
+  validates :park_name, presence: true, length: { minimum: 3}
+  validates :address, presence: true
+  validates :zip_code, presence: true
 end
