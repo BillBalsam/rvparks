7ad93e293f5a1832e5910eedd8a4152ccc075854
BillBalsam <wbalsam@knights.ucf.edu>
2021-02-04 02:36:07 +0000

    added alerts to places page when bookings overlap

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,20 +3,22 @@ class Booking < ApplicationRecord
     validates :name, presence: { message: "Name can't be blank"}
     
 
-    validate :validate_other_booking_overlap
+    validate do |comment| 
+        comment.validate_other_booking_overlap
+    end
 
     def period
         start_time..end_time
     end
 
-    private
+    
 
     def validate_other_booking_overlap
         other_bookings = Booking.all
         is_overlapping = other_bookings.any? do |other_booking|
             period.overlaps?(other_booking.period)
         end
-        # errors.add(:overlaps_with_other) if is_overlapping
+        errors.add(:base, 'Please check booking, dates cannot overlap') if is_overlapping
 
     end
 end
